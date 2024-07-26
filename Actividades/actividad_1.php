6f57693155b331f16a9857081825011c59fd375f
albasanchez00 <albasanchezolmedo00@gmail.com>
2024-07-26 09:18:09 +0200

    actividad

---
+++ Actividades/actividad_1.php
@@ -0,0 +1,12 @@
+<?php
+//Conocer la longitud de una cadena
+$cadena="Bienvenido a mi página web.";
+$primeraLetra= substr($cadena,0,1);
+echo "La primera letra es -> <strong>'$primeraLetra'</strong>";
+echo "<br>";
+$ultimaLetra= substr($cadena,strlen($primeraLetra)-3,1);
+echo "La última letra es -> <strong>'$ultimaLetra'</strong>";
+
+
+
+
